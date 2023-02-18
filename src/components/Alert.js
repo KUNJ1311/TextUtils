b07aff86fc2d7b71bdd71c5a12c3ceb7350b5534
Kunj_13 <74526794+KUNJ1311@users.noreply.github.com>
2023-02-18 16:01:53 +0530

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -5,14 +5,9 @@ function Alert(props) {
 		const lower = word.toLowerCase();
 		return lower.charAt(0).toUpperCase() + lower.slice(1);
 	};
-	const my = ()=>{
-		const we='KUNJ_FALADU1311';
-		return we;
-	}
-
+	
 	return (
 		<div style={{ height: "50px" }}>
-		<p>{my}</p>
 			{props.alert && (
 				<div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
 					<strong>{capitalize(props.alert.type)}</strong>: {props.alert.message}
